631a22ec45c8dcb6c5c3093618faf194242b5133
MiltonDuro <87989658+MiltonDuro@users.noreply.github.com>
2023-02-01 10:37:06 -0400

    Itegracion de firebase al proyecto

---
+++ DTEA/src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    firebaseConfig : {
+        apiKey: "AIzaSyAqJg-Wo_rLvVZliNSzxwIoD5FtiJsRB8o",
+        authDomain: "proyecto-final-tds.firebaseapp.com",
+        databaseURL: "https://proyecto-final-tds-default-rtdb.firebaseio.com",
+        projectId: "proyecto-final-tds",
+        storageBucket: "proyecto-final-tds.appspot.com",
+        messagingSenderId: "401823287711",
+        appId: "1:401823287711:web:a68d0c8bb99519ca3b55aa",
+        measurementId: "G-992RNX1X93"
+      }
+
+
+};
