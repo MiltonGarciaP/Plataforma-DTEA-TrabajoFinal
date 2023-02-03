b9d6b4fd438a260168f1eaf3b6919b9c1a960703
MiltonDuro <87989658+MiltonDuro@users.noreply.github.com>
2023-02-02 21:51:10 -0400

    insertar ruta de nosotros

--- DTEA/src/app/app-routing.module.ts
+++ DTEA/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
+import { MateriasComponent } from './components/materias/materias.component';
 import { NoEncontradaComponent } from './components/no-encontrada/no-encontrada.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
 import { RecuperarContraComponent } from './components/recuperar-contra/recuperar-contra.component';
 import { RegistrarUsuarioComponent } from './components/registrar-usuario/registrar-usuario.component';
 import { VerificarCorreoComponent } from './components/verificar-correo/verificar-correo.component';
@@ -14,6 +16,8 @@ const routes: Routes = [
  {path:'recuperar-contraseña', component: RecuperarContraComponent},
  {path:'principal', component: RegistrarUsuarioComponent},
  {path:'404', component: NoEncontradaComponent},
+ {path:'nosotros', component: NosotrosComponent},
+ {path:'materias', component: MateriasComponent},
  {path:'**', redirectTo:'404',pathMatch:'full'},
  
 ];
